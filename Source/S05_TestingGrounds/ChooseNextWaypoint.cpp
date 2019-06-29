1c57d7c0b9092bf03356e47726fbdeb1c4a8c3fc
DESKTOP-L1VRQGA\Owner <mknappskirata@gmail.com>
2019-06-29 16:03:24 -0500

    Code Changes

--- Source/S05_TestingGrounds/ChooseNextWaypoint.cpp
+++ Source/S05_TestingGrounds/ChooseNextWaypoint.cpp
@@ -2,9 +2,38 @@
 
 
 #include "ChooseNextWaypoint.h"
+#include "BehaviorTree/BlackboardComponent.h"
+#include "AIController.h"
+#include "PatrolRoute.h"
+#include "PatrollingGuard.h"
 
 EBTNodeResult::Type UChooseNextWaypoint::ExecuteTask(UBehaviorTreeComponent & OwnerComp, uint8 * NodeMemory)
 {
-	UE_LOG(LogTemp, Warning, TEXT("Hello there..."));
+	//TODO Protect against no patrol route component and empty patrol routes
+	auto PatrolRoute = OwnerComp.GetAIOwner()->GetPawn()->FindComponentByClass<UPatrolRoute>();
+	if (!ensure(PatrolRoute)) { return EBTNodeResult::Failed; }
+
+	//Get patrol points array
+	auto PatrollingGuard = Cast<APatrollingGuard>(OwnerComp.GetAIOwner()->GetPawn());
+	auto PatrolPoints = PatrolRoute->GetPatrolPoints();
+
+	if (PatrolPoints.Num() == 0)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("%s has an empty patrol route"), *OwnerComp.GetAIOwner()->GetPawn()->GetHumanReadableName());
+		return EBTNodeResult::Failed;
+	}
+
+
+	//Set next waypoint
+	auto BlackboardComp = OwnerComp.GetBlackboardComponent();
+	auto Index = BlackboardComp->GetValueAsInt(IndexKey.SelectedKeyName);
+	BlackboardComp->SetValueAsObject(WaypointKey.SelectedKeyName, PatrolPoints[Index]);
+
+	//Cycle index value
+	Index = (Index + 1) % PatrolPoints.Num();
+	BlackboardComp->SetValueAsInt(IndexKey.SelectedKeyName, Index);
+
 	return EBTNodeResult::Succeeded;
 }
+
+
---
+++ Source/S05_TestingGrounds/PatrolRoute.h
@@ -0,0 +1,30 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "PatrolRoute.generated.h"
+
+/**
+ * A route card to help AI choose next waypoint
+ */
+UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+class S05_TESTINGGROUNDS_API UPatrolRoute : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:	
+	TArray<AActor*> GetPatrolPoints() const { return PatrolPoints; }
+	void SetPatrolPoints(TArray<AActor*> NewPatrolRoute) { PatrolPoints = NewPatrolRoute; }
+
+
+protected:
+
+
+private:	
+	
+	UPROPERTY(EditInstanceOnly, Category = "Patrol Route CPP")
+		TArray<AActor*> PatrolPoints;
+		
+};
